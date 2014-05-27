b099281e9ec0e5a7679996f1a1a0825c25165937
Baerche <baerchen.honigsammler@gmail.com>
2014-05-27 16:17:11 +0200

    methode für austauschbare modversion entwickelt, alte versionen sammelverzeichnis, tutorial raus.

--- link-mod.sh
+++ link-mod.sh
@@ -8,15 +8,25 @@ S=$PWD
 D0=$HOME/.minetest/mods
 mkdir $D0 -p
 
-M=tutorial
+M=mb_001_pitschnass
+D=$D0/$M
+rm $D -f
+ln -s $S/versionen/$M $D
+
+M=mb_002_saeulen
+D=$D0/$M
+rm $D -f
+ln -s $S/versionen/$M $D
+
+M=wasserspiel_dev
 D=$D0/$M
 rm $D -f
 ln -s $S/$M $D
 
-M=wasserspiel
-D=$D0/$M
-rm $D -f
-ln -s $S/$M $D
+#M=mb_???_*
+#D=$D0/$M
+#rm $D -f
+#ln -s $S/versionen/$M $D0
 
 ls -l $D0
 
--- makefile
+++ makefile
@@ -1,6 +1,11 @@
+menu:
+	pkill minetest || true
+	minetest
+
 run:
 	cat makefile
 	pkill minetest || true
+	
 	minetest --go
 	#cat ~/.minetest/minetest.conf
 	
