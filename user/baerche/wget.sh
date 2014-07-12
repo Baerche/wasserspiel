1e7384625ec9a7b7186e50f348f908c55ddf16c9
baerche <baerchen.honigsammler@gmail.com>
2014-07-12 19:25:54 +0200

    wget in user/ verschoben, mobs-mod holen dazu

--- user/baerche/wget.sh
+++ user/baerche/wget.sh
@@ -2,7 +2,9 @@
 set -eu
 IFS='
 '
-cd $(dirname $0)/..
+cd $(dirname $0)/../..
+pwd
+
 
 cd downloads
 P=$HOME/.minetest/mods/my_defaultpack
@@ -10,8 +12,21 @@ mkdir -p $P
 
 
 
+M=mobs
+U=https://github.com/PilzAdam/mobs/zipball/master
+Z=$M.zip
+D=PilzAdam-mobs-c49cc47
+wget -c $U -O $Z
+trash-put $D
+unzip $Z
+trash-put $P/$M
+ln $PWD/$D $P/$M
+
+
 
 exit ### sachen drunter werden irgnoriert weil fertig
+
+
 
 #geht nicht
 #wget -c https://github.com/HybridDog/riesenpilz/archive/master.zip -O riesenpilz.zip
@@ -30,7 +45,7 @@ mv $S/$M $P/$M
 Z=minetest-craft_guide.zip
 S=minetest-craft_guide-master
 D=craft_guide
-#wget -c https://github.com/cornernote/minetest-craft_guide/archive/master.zip -O minetest-craft_guide
+#wget -c https://github.com/cornernote/minetest-craft_guide/archive/master.zip -O $Z
 trash-put $S
 unzip $Z
 trash-put $P/$D
