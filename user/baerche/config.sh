f4ce64d9418da85079cf3dab77e7077e58b55e77
baerche <baerchen.honigsammler@gmail.com>
2014-07-17 13:25:29 +0200

    kram

--- user/baerche/config.sh
+++ user/baerche/config.sh
@@ -14,7 +14,7 @@ BIN=minetest; MAP_WORLDS=$HOME/.minetest/worlds
 
 BIN=~/freeminer-4.9.3-git/bin/freeminer; MAP_WORLDS=$HOME/.freeminer/worlds; CLEANUP="xrandr --size 1280x1024"
 
-BIN=~/minetest-4.10-git/bin/minetest; MAP_WORLDS=$HOME/.minetest/worlds
+BIN=~/minetest-4.10-git/bin/minetest; MAP_WORLDS=$HOME/.minetest/worlds; CLEANUP=""
 
 
 CMD="$BIN"
